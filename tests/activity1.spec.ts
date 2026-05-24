c1a1cc2c49babbb0f746a80212a27c014b389b71
Demmi <demmi.zepeda@gmail.com>
2026-05-24 14:03:09 -0600

    first commit

---
+++ tests/activity1.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('test the page of MercadoLibre', async ({ page }) => {
+  await page.goto('https://www.mercadolibre.com.mx/');
+
+  // 1. getByRole
+  await expect(page.getByRole('link', { name: /Ingresa/i }).first()).toBeVisible();
+
+  // 2. getByTestId
+  await expect(page.getByTestId('action:understood-button')).toBeVisible();
+
+  // 3. getByAltText
+  await expect(page.getByAltText(/Mercado Libre/i).first()).toBeVisible();
+
+  // 4. getByText
+  await expect(page.getByText(/Categorías/i).first()).toBeVisible();
+
+  // 5. getByLabel
+  await expect(page.getByLabel(/Buscar/i)).toBeVisible();
+
+  // 6. getByPlaceholder
+  await expect(page.getByPlaceholder(/Buscar productos/i)).toBeVisible();
+
+  // 7. getByTitle
+  await expect(page.getByTitle(/Mercado Libre/i).first()).toBeVisible();
+});
